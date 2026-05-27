a12bc7f5d64c9beb5969f2bf8c1b902d87e772ff
Vincent TWIZERE <v.twizere@alustudent.com>
2026-05-27 21:03:19 +0200

    revert: remove middleware bug

--- src/Routes/movieRoutes.js
+++ src/Routes/movieRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { createMovie } from '../controllers/movieController.js';
-// import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
