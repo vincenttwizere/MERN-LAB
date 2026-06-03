d26fdb570cd74f7d1d92a420ac1689a356f20492
Vincent TWIZERE <v.twizere@alustudent.com>
2026-06-03 04:54:29 +0200

    chore: prepare Render deployment by adding start script and using process.env.PORT

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ app.use('/movies', movieRoutes);
 app.use('/auth', authRoutes);
 app.use('/watchlist', watchlistRoutes);
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
